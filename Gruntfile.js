5f621fbcdef79441cc9b6641b9ed0d7486d74ff8
Tomek Wytrebowicz <tomalecpub@gmail.com>
2015-03-28 23:57:36 +0100

    Add grunt bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,28 @@ module.exports = function(grunt) {
                 ],
                 // tasks: ['jshint'],
             },
+        },
+        bump: {
+          options: {
+            files: ['package.json', 'bower.json', 'src/juicy-ace-editor.html'],
+            commit: true,
+            commitMessage: '%VERSION%',
+            commitFiles: ['package.json', 'bower.json', 'src/juicy-ace-editor.html'],
+            createTag: true,
+            tagName: '%VERSION%',
+            tagMessage: 'Version %VERSION%',
+            push: false,
+            // pushTo: 'origin',
+            globalReplace: false,
+            prereleaseName: false,
+            regExp: false
+          }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bump');
 
     grunt.registerTask('server', ['connect']);
     grunt.registerTask('default', ['watch']);
